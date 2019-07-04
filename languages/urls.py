e07cca248794df18d2319006252288bcf3bf1f35
Ashutosh Panda <iamashutoshpanda@gmail.com>
2019-07-05 01:03:32 +0530

    Added 2 more models

--- languages/urls.py
+++ languages/urls.py
@@ -4,6 +4,8 @@ from rest_framework import routers
 
 router  = routers.DefaultRouter()
 router.register('languages',views.LanguageView)
+router.register('paradigm',views.ParadigmView)
+router.register('programmer',views.ProgrammerView)
 urlpatterns = [
     path('',include(router.urls))
 ]
--- languages/views.py
+++ languages/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Language
-from .serializers import LanguageSerializer
+from .models import Language,Programmer,Paradigm
+from .serializers import LanguageSerializer,ParadigmSerializer,ProgrammerSerializer
 
 class LanguageView(viewsets.ModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
+
+
+class ParadigmView(viewsets.ModelViewSet):
+    queryset = Paradigm.objects.all()
+    serializer_class = ParadigmSerializer
+
+
+class ProgrammerView(viewsets.ModelViewSet):
+    queryset = Programmer.objects.all()
+    serializer_class = ProgrammerSerializer
 
